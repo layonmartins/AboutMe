951dd37c5e6356f4aa2349cfb23f1a56c24c90e1
Layon Martins <layonmartins03@gmail.com>
2019-10-15 10:17:45 -0300

    17. Exercise: Implement Data Binding

--- app/src/main/java/com/layon/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/layon/android/aboutme/MainActivity.kt
@@ -5,30 +5,44 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.layon.android.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
+    // Instance of MyName data class
+    private val myName: MyName = MyName("Layon Martins")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = myName
 
         // Add a click handler for the Done button
-        findViewById<Button>(R.id.done_button).setOnClickListener{
+        binding.doneButton.setOnClickListener{
             addNickname(it)
         }
     }
 
     private fun addNickname(view: View){
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
 
-        nicknameTextView.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
+        binding.apply {
+            // Set the text for nicknameText to the value in nicknameEdit.
+            myName?.nickname = nicknameEdit.text.toString()
+
+            // Invalidate all binding expressions and request a new rebind to refresh UI
+            invalidateAll()
+
+            // With GONE they are invisible and do not occupy space.
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+
+            // Make the TexView with the nickname visible.
+            nicknameText.visibility = View.VISIBLE
+        }
 
         // Hide the keyboard
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
